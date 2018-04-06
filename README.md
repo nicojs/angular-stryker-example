978bdf26ee73c8fbb52ff037b2e2d6e817a6f64b
Nico Jansen <jansennico@gmail.com>
2018-04-06 11:56:58 +0200

    Update for latest stryker and angular version

--- README.md
+++ README.md
@@ -16,7 +16,8 @@ Clone this library and run `npm install`. After that, run `npm run stryker`.
 
 Follow these steps to enable Stryker mutation testing in your project.
 
-1. Install 2 webpack plugins in your project: `npm i -D angular2-template-loader awesome-typescript-loader`
+1. Install 2 webpack plugins in your project: `npm i -D angular2-template-loader awesome-typescript-loader@^3`  
+  *NOTE:* the Awesome typescript loader v3 is webpack 3 compatible. Update once the angular-cli is based on webpack 4
 1. Install stryker with plugins: `npm i -D stryker-api stryker stryker-webpack-transpiler stryker-html-reporter stryker-typescript stryker-karma-runner`
 1. Copy over the `stryker.conf.js` file and `webpack-stryker.conf.js` file from this repository.
 1. Add `"stryker": "stryker run"` to your `"scripts"` config in your package.json file.
--- stryker.conf.js
+++ stryker.conf.js
@@ -3,27 +3,8 @@ const appConfig = getAppFromConfig();
 
 module.exports = function (config) {
   config.set({
-    files: [
-      // Exclude files so we can include only particular files later on
-      '!./**/*.ts',
-      '!./**/*.js',
-      `!./${appConfig.root}/**/*.d.ts`,
-
-      // Include source files and flag them for mutation
-      { pattern: `./${appConfig.root}/**/*.ts`, included: false, mutated: true },
-
-      // Exclude test files to later include them again without the mutated flag set to true
-      `!./${appConfig.root}/**/*.spec.ts`,
-      `!./${appConfig.root}/test.ts`,
-      { pattern: `./${appConfig.root}/**/*.spec.ts`, included: false, mutated: false },
-      { pattern: `./${appConfig.root}/test.ts`, included: false, mutated: false },
-
-      // Ignore environment configuration files
-      `!./${appConfig.root}/environments/*.ts`,
-
-      // Other assets (HTML and CSS for the browser)
-      `./${appConfig.root}/**/*.html`,
-      `./${appConfig.root}/**/*.css`,
+    mutate: [
+      'src/app/*.component.ts'
     ],
     testRunner: 'karma',
     mutator: 'typescript',
@@ -33,11 +14,19 @@ module.exports = function (config) {
     coverageAnalysis: 'off',
     karmaConfig: {
       frameworks: ['jasmine'],
-      browsers: ['ChromeHeadless'],
+      customContextFile: require.resolve('@angular/cli/plugins/karma-context.html'),
+      customDebugFile: require.resolve('@angular/cli/plugins/karma-debug.html'),
       plugins: [
         require.resolve('karma-jasmine'),
-        require.resolve('karma-chrome-launcher')
-      ]
+        require.resolve('karma-chrome-launcher'),
+      ],
+      proxies: {
+        "/_karma_webpack_/": "/base/dist/"
+      },
+      files: [
+        { pattern: './dist/*.js', included: false }
+      ],
+      browsers: ['ChromeHeadless']
     },
     tsconfigFile: 'tsconfig.json',
     webpack: {
